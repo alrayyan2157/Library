b47670f804311b8b268434c7cb08415943a630fc
alrayyan2157 <alrayyan2157@icloud.com>
2026-06-06 10:31:51 +0530

    Creating array of books, object Book and func to store books

---
+++ script.js
@@ -0,0 +1,14 @@
+const bookLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    bookLibrary.push(book);
+}
